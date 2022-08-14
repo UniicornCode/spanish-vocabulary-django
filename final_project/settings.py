be2220c70f8a04612a007358d32d1c9416574f83
Natali <natalivarga25@hotmail.com>
2022-08-14 17:10:08 +0200

    Changed database from sqlite to postgres

--- final_project/settings.py
+++ final_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'spanish-vocabulary-django-db',
+        'USER': 'postgres',
+        'PASSWORD': 'natali123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
