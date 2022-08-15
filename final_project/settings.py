6c528492d35b1c46221c753687e22cd71dafb032
Natali <natalivarga25@hotmail.com>
2022-08-15 13:14:26 +0200

    Edited settings

--- final_project/settings.py
+++ final_project/settings.py
@@ -138,3 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
